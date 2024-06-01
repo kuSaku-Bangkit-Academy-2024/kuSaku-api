3590474190f905d0256342d088b8787cb2f99c61
Ilham Hakim <ilhmhakim@gmail.com>
2024-06-02 06:58:26 +0700

    ref: wallet and expense model

--- src/models/expense.js
+++ src/models/expense.js
@@ -1,7 +1,47 @@
 const ClientError = require("../utils/clientError");
 
+class Wallet {
+  constructor({ id, salary }) {
+    this.id = id;
+    this.salary = salary;
+    this.balance = salary; 
+    this.expenses = [];
+  }
+
+  addExpense(expense) {
+    this.expenses.push(expense);
+    this.calculateBalance();
+  }
+
+  calculateBalance() {
+    let totalExpenseAmount = 0;
+    for (let expense of this.expenses) {
+      totalExpenseAmount += expense.amount;
+    }
+    this.balance = this.salary - totalExpenseAmount;
+  }
+
+  toFirestore() {
+    return {
+      id: this.id,
+      salary: this.salary,
+      balance: this.balance,
+      expenses: this.expenses
+    };
+  }
+
+  toInterface() {
+    return {
+      id: this.id,
+      salary: this.salary,
+      balance: this.balance,
+      expenses: this.expenses
+    };
+  }
+}
+
 class Expense {
-  constructor({id, describe, amount, price, timestamp, category}) {
+  constructor({ id, describe, amount, price, timestamp, category }) {
     this.id = id;
     this.describe = describe;
     this.amount = amount;
@@ -14,7 +54,7 @@ class Expense {
     const requiredFields = ['id', 'describe', 'amount', 'price', 'timestamp', 'category'];
     for (let field of requiredFields) {
       if (!this[field]) {
-        if(!this.category) {
+        if (!this.category) {
           throw new ClientError(`Can't recognize the category of transaction`, 400);
         } else {
           throw new ClientError(`${field} is required`, 400);
@@ -25,24 +65,24 @@ class Expense {
 
   toFirestore() {
     return {
-        id: this.id,
-        describe: this.describe,
-        price: this.price,
-        amount: this.amount,
-        timestamp: this.timestamp,
-        category: this.category
+      id: this.id,
+      describe: this.describe,
+      price: this.price,
+      amount: this.amount,
+      timestamp: this.timestamp,
+      category: this.category
     };
   }
 
-  toInterface(){
+  toInterface() {
     return {
-        id: this.id,
-        describe: this.describe,
-        amount: this.amount,
-        timestamp: this.timestamp,
-        category: this.category
+      id: this.id,
+      describe: this.describe,
+      amount: this.amount,
+      timestamp: this.timestamp,
+      category: this.category
     };
   }
 }
 
-module.exports = Expense;
+module.exports = { Wallet, Expense };
